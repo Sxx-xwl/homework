679793dd158bd16282cf21a7ae82cadebb2d1e7e
Sxx-xwl <sxxgryx27@163.com>
2022-05-11 15:58:35 +0800

    2021.05.11刷题

---
+++ medium/src/offer0004/Offer0004.java
@@ -0,0 +1,43 @@
+package offer0004;
+
+/**
+ * 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，
+ * 每一列都按照从上到下递增的顺序排序。请完成一个高效的函数，
+ * 输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ *
+ * @author sxx_27
+ * @create 2022-05-11 13:48
+ */
+public class Offer0004 {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 4, 7, 11, 15},
+                {2, 5, 8, 12, 19},
+                {3, 6, 9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}};
+        System.out.println(new Solution().findNumberIn2DArray(matrix, 20));
+
+    }
+}
+
+class Solution {
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix.length == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+                if (matrix[i][j] > target) {
+                    break;
+                }
+            }
+        }
+
+        return false;
+    }
+}
---
+++ medium/src/offer0007/Offer0007.java
@@ -0,0 +1,132 @@
+package offer0007;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * 输入某二叉树的前序遍历和中序遍历的结果，请构建该二叉树并返回其根节点。
+ * 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+ *
+ * @author sxx_27
+ * @create 2022-05-11 13:57
+ */
+public class Offer0007 {
+
+    public static void main(String[] args) {
+
+        int[] preorder = {3, 9, 20, 15, 7};
+        int[] inorder = {9, 3, 15, 20, 7};
+        System.out.println(new Solution().buildTree(preorder, inorder));
+        System.out.println(new Solution().buildTree1(preorder, inorder));
+
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "val=" + val +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
+
+class Solution {
+    private Map<Integer, Integer> indexMap;
+
+    public TreeNode myBuildTree(int[] preorder, int[] inorder, int preorder_left,
+                                int preorder_right, int inorder_left, int inorder_right) {
+        if (preorder_left > preorder_right) {
+            return null;
+        }
+
+        // 前序遍历中的第一个节点就是根节点
+        int preorder_root = preorder_left;
+        // 在中序遍历中定位根节点
+        int inorder_root = indexMap.get(preorder[preorder_root]);
+
+        // 先把根节点建立出来
+        TreeNode root = new TreeNode(preorder[preorder_root]);
+        // 得到左子树中的节点数目
+        int size_left_subtree = inorder_root - inorder_left;
+        // 递归地构造左子树，并连接到根节点
+        // 先序遍历中「从 左边界+1 开始的 size_left_subtree」个元素就对应了中序遍历中「从 左边界 开始到 根节点定位-1」的元素
+        root.left = myBuildTree(preorder, inorder, preorder_left + 1,
+                preorder_left + size_left_subtree, inorder_left, inorder_root - 1);
+        // 递归地构造右子树，并连接到根节点
+        // 先序遍历中「从 左边界+1+左子树节点数目 开始到 右边界」的元素就对应了中序遍历中「从 根节点定位+1 到 右边界」的元素
+        root.right = myBuildTree(preorder, inorder, preorder_left + size_left_subtree + 1,
+                preorder_right, inorder_root + 1, inorder_right);
+        return root;
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        // 构造哈希映射，帮助我们快速定位根节点
+        indexMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        return myBuildTree1(preorder, inorder, 0, n - 1, 0, n - 1);
+    }
+
+    public TreeNode myBuildTree1(int[] preorder, int[] inorder, int preorder_left,
+                                 int preorder_right, int inorder_left, int inorder_right) {
+        if (preorder_left > preorder_right) {
+            return null;
+        }
+        int pre_root = preorder_left;
+        int in_root = indexMap.get(preorder[pre_root]);
+        int tree_size = in_root - inorder_left;
+        TreeNode root = new TreeNode(preorder[pre_root]);
+        root.left = myBuildTree1(preorder, inorder, preorder_left + 1,
+                preorder_left + tree_size, inorder_left, in_root - 1);
+        root.right = myBuildTree1(preorder, inorder, preorder_left + tree_size + 1,
+                preorder_right, in_root + 1, inorder_right);
+        return root;
+    }
+    public TreeNode buildTree1(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        int inorderIndex = 0;
+        for (int i = 1; i < preorder.length; i++) {
+            int preorderVal = preorder[i];
+            TreeNode node = stack.peek();
+            if (node.val != inorder[inorderIndex]) {
+                node.left = new TreeNode(preorderVal);
+                stack.push(node.left);
+            } else {
+                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]) {
+                    node = stack.pop();
+                    inorderIndex++;
+                }
+                node.right = new TreeNode(preorderVal);
+                stack.push(node.right);
+            }
+        }
+        return root;
+    }
+
+}
